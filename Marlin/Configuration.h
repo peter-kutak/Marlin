76f297fb8389e504df5e8a6ecc3b316791fa79ac
Peter Kutak <peter.kutak@gmail.com>
2023-02-03 13:55:57 +0100

    v dielni mam menej ako 5st

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -604,16 +604,17 @@
 
 // Below this temperature the heater will be switched off
 // because it probably indicates a broken thermistor wire.
-#define HEATER_0_MINTEMP   5
-#define HEATER_1_MINTEMP   5
-#define HEATER_2_MINTEMP   5
-#define HEATER_3_MINTEMP   5
-#define HEATER_4_MINTEMP   5
-#define HEATER_5_MINTEMP   5
-#define HEATER_6_MINTEMP   5
-#define HEATER_7_MINTEMP   5
-#define BED_MINTEMP        5
-#define CHAMBER_MINTEMP    5
+//chelo by to zistit skutocnu hodnotu odpojeneho senzoru
+#define HEATER_0_MINTEMP   -5
+#define HEATER_1_MINTEMP   -5
+#define HEATER_2_MINTEMP   -5
+#define HEATER_3_MINTEMP   -5
+#define HEATER_4_MINTEMP   -5
+#define HEATER_5_MINTEMP   -5
+#define HEATER_6_MINTEMP   -5
+#define HEATER_7_MINTEMP   -5
+#define BED_MINTEMP        -5
+#define CHAMBER_MINTEMP    -5
 
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
@@ -627,7 +628,7 @@
 #define HEATER_6_MAXTEMP 415
 #define HEATER_7_MAXTEMP 415
 #define BED_MAXTEMP      165
-#define CHAMBER_MAXTEMP  60
+#define CHAMBER_MAXTEMP  90
 
 /**
  * Thermal Overshoot
